be91f3f69e0ec83bb0a660118caed0c0cd613f5b
Gunnar Ahlberg <gunnara@spotify.com>
2021-10-28 20:49:57 +0200

    Added matrix moving

--- java/2020/src/com/gunnarahlberg/aoc2020/Utils.java
+++ java/2020/src/com/gunnarahlberg/aoc2020/Utils.java
@@ -14,4 +14,51 @@ public class Utils {
         return (a && !b) || (!a && b);
     }
 
+    public static int wrap(int curr, int inc, int end) {
+        int next = curr + inc;
+        if (next >= end)
+            return next - end;
+        return next;
+    }
+
+    public static IntPair newPos(IntPair start, IntPair inc, IntPair grid) {
+        // Move in a x,y coordinate using inc
+        IntPair newPos = new IntPair(start.a, start.b);
+        movePos(newPos, inc, grid);
+        return newPos;
+    }
+
+    public static void movePos(IntPair pos, IntPair inc, IntPair grid) {
+        // Move in a x,y coordinate using inc
+        pos.a = wrap(pos.a, inc.a, grid.a);
+        pos.b = wrap(pos.b, inc.b, grid.b);
+    }
+
+    public static class IntPair {
+        int a = 0, b = 0;
+
+        public IntPair() {
+        }
+
+        public IntPair(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+
+    }
+
+    public static void test() {
+        Assert.that(Utils.newPos(new IntPair(1, 1), new IntPair(3, 2), new IntPair(8, 8)).a == 4, "newPos 1");
+        Assert.that(Utils.newPos(new IntPair(1, 1), new IntPair(6, 2), new IntPair(8, 8)).a == 7, "newPos 2");
+        Assert.that(Utils.newPos(new IntPair(1, 1), new IntPair(7, 2), new IntPair(8, 8)).a == 0, "newPos 3");
+        Assert.that(Utils.newPos(new IntPair(1, 1), new IntPair(8, 1), new IntPair(8, 8)).a == 1, "newPos 4");
+        Assert.that(Utils.newPos(new IntPair(1, 1), new IntPair(9, 1), new IntPair(8, 8)).a == 2, "newPos 5");
+        Assert.that(wrap(1, 2, 4) == 3, "Wrap 2");
+
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+
 }

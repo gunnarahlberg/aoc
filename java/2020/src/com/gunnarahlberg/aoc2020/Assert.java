2f8ac13e6bf7919b0f4a54acaf6e0fbbd9836447
Gunnar Ahlberg <gunnara@spotify.com>
2021-10-28 19:04:08 +0200

    Asserts that a boolean predicate is true

---
+++ java/2020/src/com/gunnarahlberg/aoc2020/Assert.java
@@ -0,0 +1,14 @@
+package com.gunnarahlberg.aoc2020;
+
+public class Assert {
+    static void that(int actual, int expect, String msg) {
+        that(actual == expect, msg);
+    }
+
+    static void that(boolean pred, String msg) {
+        if (!pred)
+            System.err.println("Failed :" + msg);
+        else
+            System.out.println("P");
+    }
+}

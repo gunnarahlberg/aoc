828cb6f783c970d484d0e6618b8fc0996702f2f3
Gunnar Ahlberg <gunnara@spotify.com>
2021-10-28 18:33:17 +0200

    Hello world

---
+++ java/2020/src/com/gunnarahlberg/aoc2020/Day1.java
@@ -0,0 +1,9 @@
+package com.gunnarahlberg.aoc2020;
+public class Day1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
+
---
+++ java/build.fish
@@ -0,0 +1,4 @@
+rm -rf classes/*
+javac 2020/src/com/gunnarahlberg/aoc2020/* -d classes
+java -cp classes/ com.gunnarahlberg.aoc2020.Day1
+

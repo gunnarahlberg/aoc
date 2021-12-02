fcdd40f2b5a96d22ab453cb6f0d46a6e11748fcd
Gunnar Ahlberg <gunnara@spotify.com>
2021-12-02 12:16:43 +0100

    Day template

--- 2021/src/com/gunnarahlberg/aoc2021/Day.java
+++ 2021/src/com/gunnarahlberg/aoc2021/Day.java
@@ -1,31 +1,48 @@
 package com.gunnarahlberg.aoc2021;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Day {
-    static int solution1, solution2;
+    long solution1 = 0, solution2 = 0;
+    String day = getClass().getSimpleName();
+
+    static class Input {
+    }
 
     public static void main(String[] args) throws java.io.IOException {
-        List<String> sample1 = Arrays.asList(
-        );
-        List<String> input = Files.readAllLines(Path.of("2021/input", "Day?.txt"));
+        new Day2().solve();
+    }
 
+    public void solve() throws IOException {
+        long start = System.currentTimeMillis();
+        List<String> inputLines = Files.readAllLines(Path.of("2021/input", day + ".txt"));
+        List<Input> input = inputLines.stream().map(s -> parseLine(s)).collect(Collectors.toList());
         solution1 = sum1(input);
         solution2 = sum2(input);
-        System.out.printf("%s, %s%n",solution1, solution2);
-        assert solution1 == 0 : "Day1:2=" + solution2;
-        assert solution2 == 0 : "Day1:2=" + solution2;
+        long end = System.currentTimeMillis();
+        long duration = Duration.ofMillis(end - start).toMillis();
+        System.out.printf("%s in %s ms: %s, %s%n", day, duration, solution1, solution2);
+        assert solution1 == 0 : day + ":1(" + solution1 + ")";
+        assert solution2 == 0 : day + ":2(" + solution2 + ")";
     }
 
-    private static int sum1(List<String> input) {
-        return 0;
+    private static Input parseLine(String s) {
+        Input input = new Input();
+        return input;
     }
-    private static int sum2(List<String> input) {
+
+    private static long sum1(List<Input> input) {
         return 0;
     }
 
+    private static long sum2(List<Input> input) {
+        return 0;
+    }
 
 }
